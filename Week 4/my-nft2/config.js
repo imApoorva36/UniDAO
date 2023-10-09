4787681005859e4d71af6ff45999148e086d6c19
imApoorva36 <90238207+imApoorva36@users.noreply.github.com>
2023-10-09 19:34:50 +0530

    Update config.js

--- Week 4/my-nft2/config.js
+++ Week 4/my-nft2/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
-    apiKey: '26424a41fb17a5f2c416', // Replace with your Pinata API Key
-    apiSecret: '9cb79431e6f23e62050d7e81aeea038fb352e3e2c3d80f11f9a144174beb8a97', // Replace with your Pinata API Secret
+    apiKey: '', // Replace with your Pinata API Key
+    apiSecret: '', // Replace with your Pinata API Secret
     contractAddress: '0x717aF1b0983E5E44bf8E18D3Fa7BEb1dAC800cB3', // Replace with your deployed contract address
     imageFilePath: 'C:/Users/apoor/OneDrive/Desktop/my-nft2/scripts/pic.jpg', // Replace with the path to your image file
     pinataBaseURL: 'https://api.pinata.cloud',
